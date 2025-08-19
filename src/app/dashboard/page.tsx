c5d258c5008baf148799bf76b7948911f7a7e6d8
JorgeWendell <jorge.wendell@adelbr.tech>
2025-08-19 17:05:04 -0300

    feat: finish login

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,19 @@
-const DashboardPage = () => {
-    return ( <div>DashBoard</div> );
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+const DashboardPage = async () => {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+    if (!session?.user) {
+        redirect("/authentication");
+    }
+    return ( <div>
+        <h1>Dashboard</h1>
+       <h1>{session?.user?.email}</h1>
+       <h1>{session?.user?.name}</h1>
+    </div> );
 }
  
 export default DashboardPage;
